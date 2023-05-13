3b3525a5c71d59f2fdd25d68af09f209b92e0da2
Maciel19 <CaiqueMaciel-19@hotmail.com>
2023-05-13 20:19:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typed = new Typed('.text', {
+        strings: ["Front-End Developer", "Junior", ],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+
+    typed.start();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
